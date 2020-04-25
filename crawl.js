77a29352bd9da3fc2bb75c8f70a1da0d8b7e3e12
Manaus Transez <mtr@unmatched.eu>
2020-04-26 00:10:45 +0200

    Completed lyrics crawler

--- crawl.js
+++ crawl.js
@@ -2,16 +2,39 @@ global.fetch = require("node-fetch");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
-const data = [
-  {
-    id: "790jZbOfjDsMjeWpxQ0H9T",
-    title: "One",
-    artist: "Metallica",
-  },
-];
+const formatLyrics = (text) => {
+  return text
+    .replace(/\[.*?[^\)]\]/g, "") // Remove text between brackets
+    .replace(/(\r\n|\r|\n){2,}/g, "$1\n") // Remove redundant newlines
+    .trim();
+};
+
+const format = (input) => {
+  let formatted = input;
+  if (formatted.indexOf(" -") != -1) {
+    formatted = formatted.substring(0, input.indexOf(" -"));
+  }
+  if (formatted.indexOf(" (feat.") != -1) {
+    formatted = formatted.substring(0, input.indexOf(" (feat."));
+  }
+  return formatted
+    .replace("Metalocalypse: ", "")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "") // Remove accents from characters
+    .toLowerCase()
+    .split(" ")
+    .join("-")
+    .replace(/\&/g, "and")
+    .replace(/\//g, "-")
+    .replace(/\!/g, "")
+    .replace(/\?/g, "")
+    .replace(/\,/g, "")
+    .replace(/\./g, "")
+    .replace(/\'/g, "")
+    .replace(/\(|\)/g, "");
+};
 
 const writeToFile = (text, i) => {
-  console.log("Writing json to file...");
   fs.writeFile(`output/${i}.txt`, text, "utf8", (err) => {
     if (err) {
       console.error("An error occured while writing text to file.");
@@ -21,17 +44,26 @@ const writeToFile = (text, i) => {
   });
 };
 
-// Try recursive fetch when error with reformatted title
-const crawl = async (id, title, artist) => {
+// Try recursive fetch when error with reformatted name
+const crawl = async (id, name, artist) => {
+  console.log(`Fetching url ${format(artist)}-${format(name)}-lyrics`);
   const res = await fetch(
-    `https://genius.com/${artist.toLowerCase()}-${title.toLowerCase()}-lyrics`
+    `https://genius.com/${format(artist)}-${format(name)}-lyrics`
   );
   const data = await res.text();
   const $ = cheerio.load(data);
-  let lyrics = $(".lyrics");
-  writeToFile(lyrics.text().trim(), id);
+  let lyrics = $(".lyrics").text().trim();
+
+  if (lyrics == null || lyrics == "") {
+    console.log(`Lyrics not found for ${name} by ${artist}`);
+  } else {
+    writeToFile(formatLyrics(lyrics), id);
+  }
 };
 
+// Fetch all song lyrics
+const data = JSON.parse(fs.readFileSync(`output/input.json`));
+
 for (let i = 0; i < data.length; i++) {
-  crawl(data[i].id, data[i].title, data[i].artist);
+  crawl(data[i].id, data[i].name, data[i].artist);
 }
